0e1354929f63801aeda076bbb33db1ac81ecd402
Ngoc Han <phngochan1301@gmail.com>
2025-10-21 15:57:14 +0700

    Add Access Denied page and update layout for role-based dashboard navigation

---
+++ PRN222.Ass2.EVDealerSys/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace PRN222.Ass2.EVDealerSys.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
